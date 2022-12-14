677361f257ee9b7dad003953b86be8c64ddf44b7
Geoffroy OTEGBEYE <101628290+Lejeunesage@users.noreply.github.com>
2022-12-14 21:15:49 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,19 @@
-const popModalBtn = document.querySelector('.contain footer button');
-const modalContainer = document.querySelector('.modal-container');
-const closeModalBtn = document.querySelector('#closeModal');
+/********** Formulaire de connexion ***********/
 
-popModalBtn.addEventListener('click',popModal);
-closeModalBtn.addEventListener('click',closeModal);
-modalContainer.addEventListener('click',closeModal);
+let svg = document.querySelector(".svg");
+let mybtn = document.querySelector("button.user");
 
-function popModal() {
-    modalContainer.classList.add('display-modal');
+function affirst() {
+    document.querySelector(".form").style = "display: block";
 }
 
-function closeModal(event) {
-    if(event.target.id==='closeModal' || event.target.classList.contains('modal-container')) {
-        modalContainer.classList.remove('display-modal');
-    }
+function displaynone1() {
+    document.querySelector(".form").style = "display: none";
 }
+
+mybtn.addEventListener("click", function(e){
+    e.preventDefault();
+    affirst()});
+svg.addEventListener("click", function(e){
+    e.preventDefault();
+    displaynone1()});
